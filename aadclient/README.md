3549f0e2404e0187634caeecc0cae50ff59806d2
Krste Kovaceski <krste@pragdigi.com>
2019-08-15 22:13:45 +0200

    initial commot graph

---
+++ aadclient/README.md
@@ -0,0 +1,3 @@
+## graph-calls
+
+Using Azure Active Auth - http client to access data from Microsoft Graph
---
+++ aadclient/src/webparts/graphCalls/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGraphCallsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GraphCallsWebPartStrings' {
+  const strings: IGraphCallsWebPartStrings;
+  export = strings;
+}
